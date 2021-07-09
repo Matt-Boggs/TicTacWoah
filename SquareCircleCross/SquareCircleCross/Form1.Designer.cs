7279e52c6974fe974609c9183a6d0ba54a31702f
Matt-Boggs <matt.david.boggs@gmail.com>
2021-07-09 09:18:43 -0600

    Add files via upload

---
+++ SquareCircleCross/SquareCircleCross/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace SquareCircleCross
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sq1 = new System.Windows.Forms.Panel();
+            this.Sq2 = new System.Windows.Forms.Panel();
+            this.Sq3 = new System.Windows.Forms.Panel();
+            this.Sq4 = new System.Windows.Forms.Panel();
+            this.Sq5 = new System.Windows.Forms.Panel();
+            this.Sq6 = new System.Windows.Forms.Panel();
+            this.Sq7 = new System.Windows.Forms.Panel();
+            this.Sq8 = new System.Windows.Forms.Panel();
+            this.Sq9 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Sq1
+            // 
+            this.Sq1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq1.Location = new System.Drawing.Point(9, 9);
+            this.Sq1.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq1.Name = "Sq1";
+            this.Sq1.Size = new System.Drawing.Size(100, 100);
+            this.Sq1.TabIndex = 1;
+            this.Sq1.DoubleClick += new System.EventHandler(this.Sq1_DoubleClick);
+            // 
+            // Sq2
+            // 
+            this.Sq2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq2.Location = new System.Drawing.Point(109, 9);
+            this.Sq2.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq2.Name = "Sq2";
+            this.Sq2.Size = new System.Drawing.Size(100, 100);
+            this.Sq2.TabIndex = 2;
+            this.Sq2.DoubleClick += new System.EventHandler(this.Sq2_DoubleClick);
+            // 
+            // Sq3
+            // 
+            this.Sq3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq3.Location = new System.Drawing.Point(209, 9);
+            this.Sq3.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq3.Name = "Sq3";
+            this.Sq3.Size = new System.Drawing.Size(100, 100);
+            this.Sq3.TabIndex = 3;
+            this.Sq3.DoubleClick += new System.EventHandler(this.Sq3_DoubleClick);
+            // 
+            // Sq4
+            // 
+            this.Sq4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq4.Location = new System.Drawing.Point(9, 109);
+            this.Sq4.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq4.Name = "Sq4";
+            this.Sq4.Size = new System.Drawing.Size(100, 100);
+            this.Sq4.TabIndex = 4;
+            this.Sq4.DoubleClick += new System.EventHandler(this.Sq4_DoubleClick);
+            // 
+            // Sq5
+            // 
+            this.Sq5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq5.Location = new System.Drawing.Point(109, 109);
+            this.Sq5.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq5.Name = "Sq5";
+            this.Sq5.Size = new System.Drawing.Size(100, 100);
+            this.Sq5.TabIndex = 5;
+            this.Sq5.DoubleClick += new System.EventHandler(this.Sq5_DoubleClick);
+            // 
+            // Sq6
+            // 
+            this.Sq6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq6.Location = new System.Drawing.Point(209, 109);
+            this.Sq6.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq6.Name = "Sq6";
+            this.Sq6.Size = new System.Drawing.Size(100, 100);
+            this.Sq6.TabIndex = 6;
+            this.Sq6.DoubleClick += new System.EventHandler(this.Sq6_DoubleClick);
+            // 
+            // Sq7
+            // 
+            this.Sq7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq7.Location = new System.Drawing.Point(9, 209);
+            this.Sq7.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq7.Name = "Sq7";
+            this.Sq7.Size = new System.Drawing.Size(100, 100);
+            this.Sq7.TabIndex = 7;
+            this.Sq7.DoubleClick += new System.EventHandler(this.Sq7_DoubleClick);
+            // 
+            // Sq8
+            // 
+            this.Sq8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq8.Location = new System.Drawing.Point(109, 209);
+            this.Sq8.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq8.Name = "Sq8";
+            this.Sq8.Size = new System.Drawing.Size(100, 100);
+            this.Sq8.TabIndex = 8;
+            this.Sq8.DoubleClick += new System.EventHandler(this.Sq8_DoubleClick);
+            // 
+            // Sq9
+            // 
+            this.Sq9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Sq9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Sq9.Location = new System.Drawing.Point(209, 209);
+            this.Sq9.Margin = new System.Windows.Forms.Padding(0);
+            this.Sq9.Name = "Sq9";
+            this.Sq9.Size = new System.Drawing.Size(100, 100);
+            this.Sq9.TabIndex = 9;
+            this.Sq9.DoubleClick += new System.EventHandler(this.Sq9_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 513);
+            this.Controls.Add(this.Sq9);
+            this.Controls.Add(this.Sq8);
+            this.Controls.Add(this.Sq7);
+            this.Controls.Add(this.Sq6);
+            this.Controls.Add(this.Sq5);
+            this.Controls.Add(this.Sq4);
+            this.Controls.Add(this.Sq3);
+            this.Controls.Add(this.Sq2);
+            this.Controls.Add(this.Sq1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Sq1;
+        private System.Windows.Forms.Panel Sq2;
+        private System.Windows.Forms.Panel Sq3;
+        private System.Windows.Forms.Panel Sq4;
+        private System.Windows.Forms.Panel Sq5;
+        private System.Windows.Forms.Panel Sq6;
+        private System.Windows.Forms.Panel Sq7;
+        private System.Windows.Forms.Panel Sq8;
+        private System.Windows.Forms.Panel Sq9;
+    }
+}
+
---
+++ SquareCircleCross/SquareCircleCross/Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SquareCircleCross
+{
+    public partial class Form1 : Form
+    {
+        public bool MyTurn { get; set; }
+        public Pen RedPen = new Pen(Color.Red, 2);
+        public Pen BlackPen = new Pen(Color.Black, 2);
+        public Rectangle Rectangle { get; set; }
+        //public int CompAttCnt = 0;
+        public int PlayerMoves = 0;
+        //public enum Squares : int    //Not using this correctly
+        //{
+        //    Sq1,
+        //    Sq2,
+        //    Sq3,
+        //    Sq4,
+        //    Sq5,
+        //    Sq6,
+        //    Sq7,
+        //    Sq8,
+        //    Sq9
+        //}
+        
+        public List<int> possibleChoices = new List<int>()
+        {//Remove from this list as squares are claimed
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8
+        };
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void CheckWinCondition()
+        {
+            // Setup variables so if conditions are shorter, and more legible
+            var red = Color.Red;
+            var blue = Color.Blue;
+            var squareOne = Sq1.BackColor;
+            var squareTwo = Sq2.BackColor;
+            var squareThree = Sq3.BackColor;
+            var squareFour = Sq4.BackColor;
+            var squareFive = Sq5.BackColor;
+            var squareSix = Sq6.BackColor;
+            var squareSeven = Sq7.BackColor;
+            var squareEight = Sq8.BackColor;
+            var squareNine = Sq9.BackColor;
+
+            // Check blue conditions
+            //top horiz
+            if (squareOne==blue && squareTwo==blue && squareThree == blue)
+            {
+                MessageBox.Show("Blue wins!");
+                return;
+            }
+            //mid horiz
+            if (squareFour == blue && squareFive == blue && squareSix == blue)
+            {
+                MessageBox.Show("Blue wins!");
+                return;
+            }
+            //bottom horiz
+            if (squareSeven == blue && squareEight == blue && squareNine == blue)
+            {
+                MessageBox.Show("Blue wins!");
+                return;
+            }
+            //left vert
+            if (squareOne == blue && squareFour == blue && squareSeven == blue)
+            {
+                MessageBox.Show("Blue wins!");
+                return;
+            }
+            //mid vert
+            if (squareTwo == blue && squareFive == blue && squareEight == blue)
+            {
+                MessageBox.Show("Blue wins!");
+                return;
+            }
+            //right vert
+            if (squareThree == blue && squareSix == blue && squareNine == blue)
+            {
+                MessageBox.Show("Blue wins!");
+                return;
+            }
+            //upright cross
+            if (squareSeven == blue && squareFive == blue && squareThree == blue)
+            {
+                MessageBox.Show("Blue wins!");
+                return;
+            }
+            //upleft cross
+            if (squareOne == blue && squareFive == blue && squareNine == blue)
+            {
+                MessageBox.Show("Blue wins!");
+                return;
+            }
+
+
+            // Check red conditions
+            //top horiz
+            if (squareOne == red && squareTwo == red && squareThree == red)
+            {
+                MessageBox.Show("red wins!");
+                return;
+            }
+            //mid horiz
+            if (squareFour == red && squareFive == red && squareSix == red)
+            {
+                MessageBox.Show("red wins!");
+                return;
+            }
+            //bottom horiz
+            if (squareSeven == red && squareEight == red && squareNine == red)
+            {
+                MessageBox.Show("red wins!");
+                return;
+            }
+            //left vert
+            if (squareOne == red && squareFour == red && squareSeven == red)
+            {
+                MessageBox.Show("red wins!");
+                return;
+            }
+            //mid vert
+            if (squareTwo == red && squareFive == red && squareEight == red)
+            {
+                MessageBox.Show("red wins!");
+                return;
+            }
+            //right vert
+            if (squareThree == red && squareSix == red && squareNine == red)
+            {
+                MessageBox.Show("red wins!");
+                return;
+            }
+            //upright cross
+            if (squareSeven == red && squareFive == red && squareThree == red)
+            {
+                MessageBox.Show("red wins!");
+                return;
+            }
+            //upleft cross
+            if (squareOne == red && squareFive == red && squareNine == red)
+            {
+                MessageBox.Show("red wins!");
+                return;
+            }
+        }
+        public void ComputerTurn()
+        {
+            if (PlayerMoves == 5) return;
+            // Random mode first, grab a number 1-9 and check if that square is claimed
+            Random rd = new Random();
+            int choiceIndex = rd.Next(0,possibleChoices.Count);
+            int choiceActual = possibleChoices[choiceIndex];
+            ComputerAttempt(choiceActual);
+            
+        }
+        public void ComputerAttempt(int sq)
+        {
+            CheckWinCondition();
+            //CompAttCnt++;
+            //Console.WriteLine(CompAttCnt);
+            Console.WriteLine(sq);
+            switch (sq)
+            {
+                case 0:
+                    if (Sq1.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq1.BackColor = Color.Red;
+                        possibleChoices.Remove(0);
+                    }
+                    break;
+
+                case 1:
+                    if (Sq2.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq2.BackColor = Color.Red;
+                        possibleChoices.Remove(1);
+                    }
+                    break;
+                case 2:
+                    if (Sq3.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq3.BackColor = Color.Red;
+                        possibleChoices.Remove(2);
+                    }
+                    break;
+                case 3:
+                    if (Sq4.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq4.BackColor = Color.Red;
+                        possibleChoices.Remove(3);
+                    }
+                    break;
+                case 4:
+                    if (Sq5.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq5.BackColor = Color.Red;
+                        possibleChoices.Remove(4);
+                    }
+                    break;
+                case 5:
+                    if (Sq6.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq6.BackColor = Color.Red;
+                        possibleChoices.Remove(5);
+                    }
+                    break;
+                case 6:
+                    if (Sq7.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq7.BackColor = Color.Red;
+                        possibleChoices.Remove(6);
+                    }
+                    break;
+                case 7:
+                    if (Sq8.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq8.BackColor = Color.Red;
+                        possibleChoices.Remove(7);
+                    }
+                    break;
+                case 8:
+                    if (Sq9.BackColor == Color.Blue)
+                    {
+                        ComputerTurn();
+                    }
+                    else
+                    {
+                        Sq9.BackColor = Color.Red;
+                        possibleChoices.Remove(8);
+                    }
+                    break;
+                default:
+                    MessageBox.Show("No more moves!");
+                    break;
+            }
+            CheckWinCondition();
+        }
+
+        private void Sq1_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq1.ClientRectangle;
+            Sq1.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(0);
+            CheckWinCondition();
+            ComputerTurn();
+        }
+
+        private void Sq2_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq2.ClientRectangle;
+            Sq2.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(1);
+            CheckWinCondition();
+            ComputerTurn();
+
+        }
+
+        private void Sq3_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq3.ClientRectangle;
+            Sq3.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(2);
+            CheckWinCondition();
+            ComputerTurn();
+
+        }
+
+        private void Sq4_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq4.ClientRectangle;
+            Sq4.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(3);
+            CheckWinCondition();
+            ComputerTurn();
+
+        }
+
+        private void Sq5_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq5.ClientRectangle;
+            Sq5.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(4);
+            CheckWinCondition();
+            ComputerTurn();
+
+        }
+
+        private void Sq6_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq6.ClientRectangle;
+            Sq6.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(5);
+            CheckWinCondition();
+            ComputerTurn();
+
+        }
+
+        private void Sq7_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq7.ClientRectangle;
+            Sq7.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(6);
+            CheckWinCondition();
+            ComputerTurn();
+
+        }
+
+        private void Sq8_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq8.ClientRectangle;
+            Sq8.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(7);
+            CheckWinCondition();
+            ComputerTurn();
+
+        }
+
+        private void Sq9_DoubleClick(object sender, EventArgs e)
+        {
+            Rectangle = Sq9.ClientRectangle;
+            Sq9.BackColor = Color.Blue;
+            MyTurn = false;
+            PlayerMoves++;
+            possibleChoices.Remove(8);
+            CheckWinCondition();
+            ComputerTurn();
+
+        }
+    }
+}
